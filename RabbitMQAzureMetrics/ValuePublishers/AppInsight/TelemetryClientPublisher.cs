e78242a89cdbfe3468702002924ca9a3891f583b
Bouke Haarsma <bhaarsma@cloudbilling.nl>
2023-10-07 11:47:51 +0200

    Warn when a metric could not be published

--- RabbitMQAzureMetrics/ValuePublishers/AppInsight/TelemetryClientPublisher.cs
+++ RabbitMQAzureMetrics/ValuePublishers/AppInsight/TelemetryClientPublisher.cs
@@ -1,4 +1,6 @@
-﻿namespace RabbitMQAzureMetrics.ValuePublishers.AppInsight
+﻿using Microsoft.Extensions.Logging;
+
+namespace RabbitMQAzureMetrics.ValuePublishers.AppInsight
 {
     using System;
     using System.Threading.Tasks;
@@ -8,10 +10,12 @@
     public class TelemetryClientPublisher : IMetricPublisher
     {
         private readonly Metric metric;
+        private readonly ILogger logger;
 
-        public TelemetryClientPublisher(Metric metric)
+        public TelemetryClientPublisher(Metric metric, ILogger logger)
         {
             this.metric = metric;
+            this.logger = logger;
         }
 
         public Task PublishAsync(MetricValueCollectionWrapper collector)
@@ -33,6 +37,7 @@
 
             var method = this.metric.GetType().GetMethod(nameof(this.metric.TrackValue), types);
             var parameters = new object[types.Length];
+            var result = true;
 
             foreach (var itm in collector.Values)
             {
@@ -42,7 +47,14 @@
                     parameters[i + 1] = itm.Dimensions[i];
                 }
 
-                method.Invoke(this.metric, parameters);
+                result &= (bool?)method.Invoke(this.metric, parameters) ?? true;
+            }
+
+            if (!result)
+            {
+                logger.LogError(
+                    "Could not publish all metrics, limits reached? Series count: {SeriesCount}, Collector count: {CollectorCount}",
+                    metric.SeriesCount, values.Count);
             }
 
             return Task.CompletedTask;
