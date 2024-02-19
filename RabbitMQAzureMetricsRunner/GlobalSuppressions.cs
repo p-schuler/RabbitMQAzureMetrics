4cd9b1c685fd0a77c05011f63f921624ab7c902e
Bouke Haarsma <bhaarsma@cloudbilling.nl>
2024-02-19 07:31:56 +0100

    Upgrade StyleCop.Analyzers to 1.2.0-beta.556

--- RabbitMQAzureMetricsRunner/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Skipping documentation for sample code")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File should have header", Justification = "Skipping headers for sample code")]
